77f00eb0f042a7419478a90f18d5fb3ffe5bf3f5
Shayan <shayansadeghi178@gmail.com>
2021-10-27 09:13:11 -0700

    new changes

--- MemoryGame.py
+++ MemoryGame.py
@@ -1,6 +1,6 @@
 #shayansdq changed this file
 # This is a sample Python script.
-
+#hello
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import pygame, sys, random
